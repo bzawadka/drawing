19025a7f2b42e5ebb6250622861d5d037715d8d4
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2017-02-19 00:37:36 +0100

    canvas stub

---
+++ src/main/java/pl/bzawadka/Canvas.java
@@ -0,0 +1,17 @@
+package pl.bzawadka;
+
+public class Canvas {
+
+    public Canvas(int width, int height) {
+
+    }
+
+    public String draw() {
+        return "----------------------\n" +
+               "|                    |\n" +
+               "|                    |\n" +
+               "|                    |\n" +
+               "|                    |\n" +
+               "----------------------";
+    }
+}
--- src/test/java/pl/bzawadka/CanvasTest.java
+++ src/test/java/pl/bzawadka/CanvasTest.java
@@ -1,14 +1,33 @@
 package pl.bzawadka;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CanvasTest {
 
     @Test
     public void canvasOfCorrectSizeIsDrawn() {
-        // create canvas object 20 x 4
-        // draw canvas
-        // compare output with canvas.txt
+        Canvas canvas = new Canvas(20, 4);
+        String expectedCanvasOutput = readFile("canvas.txt");
+        assertThat(canvas.draw()).isEqualTo(expectedCanvasOutput);
+    }
+
+    private String readFile(String fileName) {
+        String content = null;
+        try {
+            URL resource = this.getClass().getResource(fileName);
+            content = FileUtils.readFileToString(new File(resource.toURI()));
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return content;
     }
 
 }
