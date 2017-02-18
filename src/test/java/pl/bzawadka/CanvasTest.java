7044bf5385cca8bb71eb7e0733d52a15bbdd03ed
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2017-02-19 00:24:39 +0100

    canvas test

---
+++ src/test/java/pl/bzawadka/CanvasTest.java
@@ -0,0 +1,14 @@
+package pl.bzawadka;
+
+import org.junit.Test;
+
+public class CanvasTest {
+
+    @Test
+    public void canvasOfCorrectSizeIsDrawn() {
+        // create canvas object 20 x 4
+        // draw canvas
+        // compare output with canvas.txt
+    }
+
+}
