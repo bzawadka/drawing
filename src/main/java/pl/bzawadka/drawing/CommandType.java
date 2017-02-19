5cf562200c6111070973176962210b0c90bfad8e
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2017-02-19 19:35:53 +0100

    command: type

---
+++ src/main/java/pl/bzawadka/drawing/CommandType.java
@@ -0,0 +1,23 @@
+package pl.bzawadka.drawing;
+
+public enum CommandType {
+    CREATE_CANVAS('C'),
+    DRAW_LINE('L'),
+    DRAW_RECTANGLE('R'),
+    BRUSH('B'),
+    QUIT('Q');
+
+    private Character code;
+
+    CommandType(Character code) {
+        this.code = code;
+    }
+
+    public static CommandType parseFrom(char code) {
+        for (CommandType type : CommandType.values()) {
+            if (type.code.toString().equalsIgnoreCase(String.valueOf(code)))
+                return type;
+        }
+        throw new IllegalArgumentException("Unrecognized command: " + code);
+    }
+}
---
+++ src/test/java/pl/bzawadka/drawing/CommandTypeTest.java
@@ -0,0 +1,29 @@
+package pl.bzawadka.drawing;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.StrictAssertions.assertThatThrownBy;
+import static pl.bzawadka.drawing.CommandType.*;
+
+public class CommandTypeTest {
+
+    @Test
+    public void commandCharacterIsChecked() {
+        assertThatThrownBy(() -> CommandType.parseFrom('Z'))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Unrecognized command: Z");
+    }
+
+    @Test
+    public void commandCanBeParsed() {
+        assertThat(CommandType.parseFrom('C')).isEqualTo(CREATE_CANVAS);
+        assertThat(CommandType.parseFrom('c')).isEqualTo(CREATE_CANVAS);
+        assertThat(CommandType.parseFrom('L')).isEqualTo(DRAW_LINE);
+        assertThat(CommandType.parseFrom('l')).isEqualTo(DRAW_LINE);
+        assertThat(CommandType.parseFrom('R')).isEqualTo(DRAW_RECTANGLE);
+        assertThat(CommandType.parseFrom('r')).isEqualTo(DRAW_RECTANGLE);
+        assertThat(CommandType.parseFrom('B')).isEqualTo(BRUSH);
+        assertThat(CommandType.parseFrom('b')).isEqualTo(BRUSH);
+    }
+}
