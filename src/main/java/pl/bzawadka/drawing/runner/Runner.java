7222870f0ea0de4f49abe11badfb1f24772b498e
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2017-03-25 23:16:12 +0100

    BucketFillCommand usage

--- src/main/java/pl/bzawadka/drawing/runner/Runner.java
+++ src/main/java/pl/bzawadka/drawing/runner/Runner.java
@@ -3,7 +3,6 @@ package pl.bzawadka.drawing.runner;
 import pl.bzawadka.drawing.Canvas;
 import pl.bzawadka.drawing.CommandFactory;
 import pl.bzawadka.drawing.command.Command;
-import pl.bzawadka.drawing.shapes.Point;
 
 import java.io.InputStream;
 import java.io.PrintStream;
@@ -11,7 +10,6 @@ import java.util.List;
 import java.util.Scanner;
 
 import static java.util.Objects.requireNonNull;
-import static pl.bzawadka.drawing.shapes.Point.point;
 
 public class Runner {
     private static final int DEFAULT_CANVAS_WIDTH = 20;
@@ -47,12 +45,6 @@ public class Runner {
                 switch (simpleCommand.commandType) {
                     case CREATE_CANVAS:
                         canvas = Canvas.canvas(args.get(0), args.get(1));
-                        break;
-                    case BUCKET_FILL:
-                        Point startingPoint = point(args.get(0), args.get(1));
-                        canvas.bucketFill(startingPoint, simpleCommand.character.get());
-                        break;
-                    case QUIT:
                         break;
                 }
             }
